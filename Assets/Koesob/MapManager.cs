e144c710157ea8148c86493276bc73959112a1db
Koesob <epi3706@khu.ac.kr>
2023-03-30 20:11:31 +0900

    기본 세팅

---
+++ Assets/Koesob/MapManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+
+public partial class MapManager
+{
+    public void GetTile(int x, int y)
+    {
+
+    }
+}
---
+++ Assets/Koesob/Tile.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public partial class MapManager
+{
+    public class Tile
+    {
+    }
+}   
+
