4313c9a5e95db7d9ec09d59ac1eaf77be7c69221
Koesob <epi3706@khu.ac.kr>
2023-04-02 22:54:40 +0900

    GameManager, MapManager, Tile, Chip

--- Assets/Koesob/MapManager.cs
+++ Assets/Koesob/MapManager.cs
@@ -3,8 +3,61 @@ using System.Collections.Generic;
 
 public partial class MapManager
 {
-    public void GetTile(int x, int y)
+    public struct TileRow
     {
+        public List<Tile> rowTile;
+    }
 
+    public List<TileRow> tiles;
+
+    public List<TileRow> Tiles { get => tiles; set => tiles = value; }
+
+    public void Init()
+    {
+        UnityEngine.Debug.LogWarning("MapManager Init");
+
+        Tiles = new List<TileRow>();
+
+        for(int i = 0; i < 5; i++)
+        {
+            Tile tempTile = new Tile(i.ToString());
+            TileRow tempTileRow = new TileRow();
+            tempTileRow.rowTile = new List<Tile>();
+            tempTileRow.rowTile.Add(tempTile);
+            Tiles.Add(tempTileRow);
+        }
+
+        Debug();
+    }
+
+    public Tile GetTile(int x, int y)
+    {
+        return Tiles[x].rowTile[y];
+    }
+
+    public Chip MakeChip()
+    {
+        return new Chip();
+    }
+
+    public void Debug()
+    {
+        int index = 0;
+        foreach(var rowTile in tiles)
+        {
+            string debug = "rowTile" + index;
+            foreach(var tile in rowTile.rowTile)
+            {
+                debug += " " + tile.ID;
+
+                foreach (var chip in tile.Chips)
+                {
+                    debug += " " + "C";
+                }
+            }
+
+            UnityEngine.Debug.LogWarning(debug);
+            index++;
+        }
     }
 }
--- Assets/Koesob/Tile.cs
+++ Assets/Koesob/Tile.cs
@@ -6,6 +6,28 @@ public partial class MapManager
 {
     public class Tile
     {
+        private string id;
+        private Stack<Chip> chips;
+
+        public string ID { get { return id; } }
+        public Stack<Chip> Chips { get { return chips; } }
+
+        public Tile(string id)
+        {
+            this.id = id;
+
+            chips = new Stack<Chip>();
+        }
+
+        public Chip Pop()
+        {
+            return chips.Pop();
+        }
+
+        public void Push(Chip _chip)
+        {
+            chips.Push(_chip);
+        }
     }
-}   
+}
 
